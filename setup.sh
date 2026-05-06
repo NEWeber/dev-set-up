6298da5b1fdc5b3cecf9b816ca6ba2c148f5a48d
Nicholas Weber <nicholaseweber@gmail.com>
2026-05-06 14:04:44 -0500

    Move bashrc into folder, update setup script
    Using the ~/code/playground approach so that this is suitable for work computers and the git config that I'll set up in the next commit

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# After installing git, setting up GitHub SSH Key and VS Code, make a ~/code directory
+# After installing git, setting up GitHub SSH Key and VS Code, make a ~/code/playground directory: this will be where your personal work lives
+cd ~/code/playground
 git clone git@github.com:NEWeber/dev-set-up.git 
 cd ~/dev-set-up
-ln -s ~/code/dev-set-up/.gitconfig ~/.gitconfig
-ln -s ~/code/dev-set-up/.bashrc ~/.bashrc
+ln -s ~/code/playground/dev-set-up/.gitconfig ~/.gitconfig
+ln -s ~/code/playground/dev-set-up/bash/.bashrc ~/.bashrc
 xargs -n 1 code --install-extension < ./vscode/vscode-extensions.txt
